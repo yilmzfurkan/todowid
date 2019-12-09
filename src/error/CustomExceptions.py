5d8b7ea1d03f88a8dbdad6e17defe15171cbba8b
Orhan Tuğrul <44259675+orhantgrl@users.noreply.github.com>
2019-12-09 16:27:11 +0300

    Update CustomExceptions.py

--- src/error/CustomExceptions.py
+++ src/error/CustomExceptions.py
@@ -1,5 +1,5 @@
 class TodoNotFoundException(Exception):
-    """Raised when when there is no to do item"""
+    """Raised when there is no to do item"""
 
     def __init__(self, message):
         super().__init__(message)
