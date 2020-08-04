338edf786da6c7e2cc9546ed14852e6c75fe29a5
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-08-04 16:12:33 +0300

    Ignore __pycache__ files and removed main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import urwid
-import peewee
-from src.model import Todo
-from src.model.Todo import Todo as TodoClass
-
-if __name__ == "__main__":
-    Todo.db.connect()
-    Todo.db.create_tables([TodoClass])
