cb93b45715f80d3442eb6dc7a5d71edcb5fec982
Orhan Tugrul <orhan.tugrul.61@gmail.com>
2020-08-07 22:01:47 +0300

    Create header layout of main window

---
+++ src/ui/layout/Header.py
@@ -0,0 +1,28 @@
+import urwid
+from src.ui.layout.Layout import Layout
+
+
+# Created by orhantgrl
+# created on 8/6/20
+
+class Header(Layout):
+    def __init__(self, title, subtitle):
+        """
+        :type title str
+        :type subtitle str
+        """
+        super().__init__()
+        self.title = title
+        self.subtitle = subtitle
+
+    def __draw__(self):
+        title = urwid.BigText(self.title, urwid.HalfBlock5x4Font())
+        date = urwid.Text(self.subtitle, align=urwid.CENTER)
+
+        columns = urwid.Pile([
+            urwid.AttrMap(urwid.Padding(w=title, align=urwid.CENTER, width=urwid.CLIP), 'head'),
+            urwid.Padding(date),
+        ])
+
+        filler = urwid.Filler(body=columns, top=4, bottom=2)
+        return urwid.Padding(w=urwid.BoxAdapter(box_widget=filler, height=8), left=1, right=1)
